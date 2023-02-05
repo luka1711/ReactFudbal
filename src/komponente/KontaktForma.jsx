cb854fbe77ac150d4e077b8955d3bd92941b5072
luka1711 <lukaraska@gmail.com>
2023-02-05 12:32:11 +0100

    Ure]enje dizajna

--- src/komponente/KontaktForma.jsx
+++ src/komponente/KontaktForma.jsx
@@ -17,24 +17,24 @@ function KontaktForma(props) {
     function handleUpit(e) {
         setUpit(e.target.value);
     }
-
+    
     return (
         <div>
             <form className="form">
                 <h3> {props.tekst}</h3>
                 <div className="element">
-                    <label>Unesite ime: </label>
+                    <label><b>Unesite ime:</b></label>
                     <input type="text" className="form-control" value={ime} onChange={handleIme}></input>
                 </div>
                 <div className="element">
-                    <label>Unesite email: </label>
+                    <label><b>Unesite email:</b> </label>
                     <input type="email" className="form-control" value={email} onChange={handleEmail}></input>
                 </div>
                 <div className="element">
-                    <label>Unesite upit: </label>
+                    <label><b>Unesite upit: </b></label>
                     <input type="text" className="form-control" value={upit} onChange={handleUpit}></input>
                 </div>
-                <button className="btn btn-success btn-sm" type="submit" onClick={() => props.kontaktiraj(ime,email)} value={email} onChange={handleEmail}>Posalji</button>
+                <button className="btn btn-success btn-sm" type="submit" onClick={() => props.kontaktiraj(ime,email)} value={email} onChange={handleEmail}><b>Posalji</b></button>
             </form>
         </div>
     );
--- src/komponente/NavBar.jsx
+++ src/komponente/NavBar.jsx
@@ -25,12 +25,12 @@ function NavBar({dobitnici}) {
         <div>
            <nav className="navbar navbar-expand-xl navbar-light bg-light ">
                 <div className="container-fluid">
-                    <Link className="navbar-brand" to="/"> Pocetna</Link>
+                    <Link className="navbar-brand" to="/"> <b>Pocetna</b></Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarBasic"
                             aria-controls="navbarBasic" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
-                    <Link className="navbar-brand" to="/kontaktiraj"> Kontakt </Link>
+                    <Link className="navbar-brand" to="/kontaktiraj"> <b>Kontakt</b> </Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarBasic"
                             aria-controls="navbarBasic" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
