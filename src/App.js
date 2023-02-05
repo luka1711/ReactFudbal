bee47f6b12bcef59d91e90a4d167c22382900eeb
luka1711 <lukaraska@gmail.com>
2023-02-05 12:10:11 +0100

    Navigacija na stranici i pretraga

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Pocetna from './komponente/Pocetna';
 import Kontakt from './komponente/Kontakt';
+import NavBar from './komponente/NavBar';
 
 function App() {
+
+  const dobitnici = [
+    { ime: 'Luka Veljovic', email: 'lukaraska@gmail.com'},
+    { ime: 'Petar Petrovic', email: 'petarp@gmail.com'},
+    { ime: 'Tadija Tadic', email: 'tadijat@gmail.com'},
+  ];
   
   return(
     <div className>
       <Router>
+        <NavBar  dobitnici={dobitnici}/>
         <Routes>
           <Route path="/" element={<Pocetna/>} />
           <Route path="/kontaktiraj" element={<Kontakt/>} />
---
+++ src/komponente/NavBar.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+function NavBar({dobitnici}) {
+    const [email, setEmail] = useState('');
+
+    function handleEmail(e) {
+        setEmail(e.target.value);
+    }
+
+    function proveri(e) {
+        var element = dobitnici.filter(function (d) {
+            return d.email == email;
+        })
+        if (element.length !== 0) {
+            alert('Čestitamo osvojili ste karte!');
+        }
+        else {
+            alert('Više sreće drugi put! Nastavite da nas pratite!')
+        }
+    }
+
+    return (
+        <div>
+           <nav className="navbar navbar-expand-xl navbar-light bg-light ">
+                <div className="container-fluid">
+                    <Link className="navbar-brand" to="/"> Pocetna</Link>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarBasic"
+                            aria-controls="navbarBasic" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <Link className="navbar-brand" to="/kontaktiraj"> Kontakt </Link>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarBasic"
+                            aria-controls="navbarBasic" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="element">
+                        <label id="tekst-pretrage">Unesite Vaš email kako biste proverili da li ste osvojili karte za utakmicu: </label>
+                        <input type="text" className="form-control" value={email} onChange={handleEmail}></input>
+                        <button type="button" onClick={proveri} className="btn btn-success btn-sm" value={email}>Proveri</button>
+                    </div>
+                </div>
+            </nav>
+        </div >
+    );
+}
+
+export default NavBar
